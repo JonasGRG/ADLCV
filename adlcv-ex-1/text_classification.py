d16b9b3ab6b1bbf2e8fb9e1f52ae62e5508d75f8
JonasGRG <jonas@gjerris.dk>
2024-01-31 20:08:41 +0100

    fixed minor bug

--- adlcv-ex-1/text_classification.py
+++ adlcv-ex-1/text_classification.py
@@ -129,4 +129,4 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  
     print(f"Model will run on {device}")
     set_seed(seed=1)
-    main()
+    main(args)
